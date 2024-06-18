c823f38234a13693216848634c4eccef8e6e6ea5
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-06-18 16:18:59 +0200

    chap routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
+;
 
 
 @Component({
@@ -8,26 +7,4 @@ import { Pokemon } from './pokemon';
   templateUrl:'app.compenent.html'
 
 })
-export class AppComponent implements OnInit{
-  pokemonList:Pokemon[] = POKEMONS
-  pokemonSelected: Pokemon| undefined
-
-  ngOnInit(){
-    console.table(this.pokemonList)
-    
-  }
-
-  selectPokemon(pokemonId: string){
-    const pokemon : Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    
-    if(pokemon){
-       console.log(`Vous avez demandé le pokémon ${pokemon.name}`)
-       this.pokemonSelected = pokemon
-    }
-    else{
-      console.log(`Vous avez demandé un pokémon qui n'existe pas!`)
-      this.pokemonSelected = pokemon
-   }
-   
-  }
-}
+export class AppComponent{}
